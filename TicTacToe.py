d7e6b8a4ad32d3b02b25e479afc0587cd19ddc27
Rares G <graresn@yahoo.com>
2023-01-17 13:26:34 +0200

    Define player input and board logic
    Definition of turns, players, inputs

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,29 +1,49 @@
 import random
 
 class TicTacToe:
-
+#------------------Game initialization------------------------
     def __init__(game):
         game.board = []
-
+#------------------Board design-------------------------------
     def board_create(game):
         for i in range(3):
             row = []
             for j in range(3):
                 row.append('-')
             game.board.append(row)
-
+#------------------Board Generation--------------------------
     def show_board(game):
         for row in game.board:
             for item in row:
                 print(item, end=" ")
             print()
+#------------------Define First Player-----------------------
+    def get_random_first_player(game):
+        return random.randint(0, 1)
+#------------------Define Player Spot------------------------
+    def fix_spot(game, row, col, player):
+        game.board[row][col] = player
+#------------------Define Board Check------------------------
+    def is_board_filled(game):
+            for row in game.board:
+                for item in row:
+                    if item == '-':
+                        return False
+            return True
 
+#------------------Board Logic-----------------------------
     def start(game):
         game.board_create()
-        
-        game.show_board()
-
-
-# starting the game
+#------------------Player's Turn---------------------------
+        player = 'X' if game.get_random_first_player() == 1 else 'O'
+        while True:
+            print(f"Player {player} turn")
+#------------------Show Board------------------------------
+            game.show_board()
+#------------------Recieve Player Input--------------------
+            row, col = list(
+                map(int, input("Enter row and column numbers to fix spot: ").split()))
+            print()
+#---------------------Start Game-----------------------------
 tic_tac_toe = TicTacToe()
 tic_tac_toe.start()
