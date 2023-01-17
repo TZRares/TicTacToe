db9f3e87fb8f14f27e0705ab66acbf05e8c0b6ae
Rares G <graresn@yahoo.com>
2023-01-17 13:17:50 +0200

    Design of the board
    Creation of the visual board

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,10 +1,29 @@
-# tic_tac_toe.py
+import random
 
-import tkinter as tk
-from tkinter import font
+class TicTacToe:
 
-class TicTacToeBoard(tk.Tk):
-    def __init__(self):
-        super().__init__()
-        self.title("Tic-Tac-Toe Game")
-        self._cells = {}
+    def __init__(game):
+        game.board = []
+
+    def board_create(game):
+        for i in range(3):
+            row = []
+            for j in range(3):
+                row.append('-')
+            game.board.append(row)
+
+    def show_board(game):
+        for row in game.board:
+            for item in row:
+                print(item, end=" ")
+            print()
+
+    def start(game):
+        game.board_create()
+        
+        game.show_board()
+
+
+# starting the game
+tic_tac_toe = TicTacToe()
+tic_tac_toe.start()
