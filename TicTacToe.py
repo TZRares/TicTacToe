689527174d8aba938e8aa4a18813c82637206e32
Rares G <graresn@yahoo.com>
2023-01-17 12:53:25 +0200

    Initial commit

---
+++ TicTacToe.py
@@ -0,0 +1,10 @@
+# tic_tac_toe.py
+
+import tkinter as tk
+from tkinter import font
+
+class TicTacToeBoard(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Tic-Tac-Toe Game")
+        self._cells = {}
